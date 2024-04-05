cecf7cb35f63413d5b1374e883440cc7ddcee5ed
jamber751 <jaamber751@gmail.com>
2024-04-05 18:37:44 +0500

    fixup! add empty queue check for P1

--- Task3/Task3.xaml.cs
+++ Task3/Task3.xaml.cs
@@ -115,7 +115,7 @@ public partial class Task3 : ContentPage
                     stopProccessors();
                     return;
                 }
-                processor2Timer.Change(0, Timeout.Infinite);
+                processor1Timer.Change(0, Timeout.Infinite);
                 return;
             }
 
